4b9ca6176cc26bdafdc82fb2e97cbdf6a4e841f1
derry21378 <domi24domi@gmail.com>
2026-03-01 19:51:01 +0100

    i require meth

--- Scripts/card_item_symbol.gd
+++ Scripts/card_item_symbol.gd
@@ -6,7 +6,7 @@ const CARD_NOT_BUTTON = preload("uid://cn1w3p2habt0h")
 func _on_pressed() -> void:
 	var instance = CARD_NOT_BUTTON.instantiate()
 	add_sibling(instance)
-	instance.position = Vector2(103.7, 161)
+	instance.position = Vector2(-243, 11)
 	print("toalteta")
 	visible = false
 	disabled = true
--- Scripts/card_not_button.gd
+++ Scripts/card_not_button.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends Node2D
 
 @onready var card_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
