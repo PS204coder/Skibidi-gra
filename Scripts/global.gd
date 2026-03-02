47e0a0d86cd37c0c22554794b681eb82030b6021
PS204coder <psulej10@gmail.com>
2026-03-02 11:19:34 +0100

    some little changes

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,6 +1,13 @@
 extends Node
 "A global code for passing aroud variables and statistics"
 
+#Statistics
 var gremlins_killed: int
 var experience: int
+
+#Values
 var attack_value_def_bullet = 7
+
+#Inventories
+var active_cards = []
+var inventory = []
